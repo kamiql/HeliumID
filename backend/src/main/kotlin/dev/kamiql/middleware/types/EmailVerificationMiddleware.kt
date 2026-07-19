377d1a8981abbbb93de7305b41e0ba96eb7ff5e0
kamiql <kilianaqua08@gmail.com>
2026-07-19 20:28:20 +0200

    fix email middleware

--- backend/src/main/kotlin/dev/kamiql/middleware/types/EmailVerificationMiddleware.kt
+++ backend/src/main/kotlin/dev/kamiql/middleware/types/EmailVerificationMiddleware.kt
@@ -1,16 +1,20 @@
 package dev.kamiql.middleware.types
 
-import dev.kamiql.UserAttributeKey
+import dev.kamiql.domain.session.UserSession
 import dev.kamiql.middleware.Middleware
 import dev.kamiql.middleware.MiddlewareResult
+import dev.kamiql.storage.UserRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
 
 object EmailVerificationMiddleware : Middleware {
     override val priority: Int = 1
 
     override suspend fun execute(call: ApplicationCall): MiddlewareResult {
-        val user = call.attributes[UserAttributeKey]
+        val session = call.sessions.get<UserSession>() ?: return MiddlewareResult.Pass
+        val user = UserRepository[session.userId] ?: return MiddlewareResult.Pass
 
         if (!user.emailVerified) return MiddlewareResult.Block(
             HttpStatusCode.Unauthorized,
